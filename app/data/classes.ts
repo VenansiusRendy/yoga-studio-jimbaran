1d27906f3105f7704bbd0211781429987e3e3f3a
Rendy Setiawan <lierendysetiawan3232@gmail.com>
2026-02-03 15:35:02 +0800

    Update Classes

--- app/data/classes.ts
+++ app/data/classes.ts
@@ -1,52 +1,52 @@
 export const classes = [
-	{
-		id: 1,
-		time: "31 January 2026 | 08.00–09.15 WITA",
-		type: "All Body Flow",
-		level: "All Levels",
-		instructor: "Tatjana",
-		duration: 75,
-		description:
-			"Vinyasa style all body flow led by Tatjana. Suitable for all levels of yoga practitioners. Includes 75 minutes of vinyasa flow, 10% discount on all NUI Healthy Food menus, and yoga mats available in limited quantity.",
-		booking: "https://kuyy.id/8c430e01-c147-41ce-b065-aeb47eae1cb3",
-		typeColor: "",
-	},
-	{
-		id: 2,
-		time: "1 February 2026 | 15.00–16.15 WITA",
-		type: "Private Wellness Coaching",
-		level: "All Levels",
-		instructor: "Saut",
-		duration: 75,
-		description:
-			"Combination of yoga and Thai massage designed to stretch, relax, and recharge. Helps melt away tension and rejuvenate both body and mind.",
-		booking: "https://kuyy.id/70732cbd-331e-43ff-b4b5-e8067ce8c282",
-	},
-	{
-		id: 7,
-		time: "2 February 2026 | 17.30–18.30 WITA",
-		type: "Sore Flow with NUI",
-		level: "All Levels",
-		instructor: "Saut",
-		duration: 60,
-		description:
-			"Yoga flow to help relieve fatigue, release stress, and support overall well-being.",
-		booking: "https://kuyy.id/a6346c09-b758-4e28-b954-22baaf78e88a",
-	},
-	{
-		id: 9,
-		time: "2 February 2026 | 19.00–20.00 WITA",
-		type: "After Work Yoga",
-		level: "All Levels",
-		instructor: "Saut",
-		duration: 60,
-		description:
-			"Yoga session focused on reducing tiredness and releasing stress after a long day.",
-		booking: "https://kuyy.id/c66b6889-e369-4d27-9def-8251a39acea4",
-	},
+	// {
+	// 	id: 1,
+	// 	time: "31 January 2026 | 08.00–09.15 WITA",
+	// 	type: "All Body Flow",
+	// 	level: "All Levels",
+	// 	instructor: "Tatjana",
+	// 	duration: 75,
+	// 	description:
+	// 		"Vinyasa style all body flow led by Tatjana. Suitable for all levels of yoga practitioners. Includes 75 minutes of vinyasa flow, 10% discount on all NUI Healthy Food menus, and yoga mats available in limited quantity.",
+	// 	booking: "https://kuyy.id/8c430e01-c147-41ce-b065-aeb47eae1cb3",
+	// 	typeColor: "",
+	// },
+	// {
+	// 	id: 2,
+	// 	time: "1 February 2026 | 15.00–16.15 WITA",
+	// 	type: "Private Wellness Coaching",
+	// 	level: "All Levels",
+	// 	instructor: "Saut",
+	// 	duration: 75,
+	// 	description:
+	// 		"Combination of yoga and Thai massage designed to stretch, relax, and recharge. Helps melt away tension and rejuvenate both body and mind.",
+	// 	booking: "https://kuyy.id/70732cbd-331e-43ff-b4b5-e8067ce8c282",
+	// },
+	// {
+	// 	id: 7,
+	// 	time: "2 February 2026 | 17.30–18.30 WITA",
+	// 	type: "Sore Flow with NUI",
+	// 	level: "All Levels",
+	// 	instructor: "Saut",
+	// 	duration: 60,
+	// 	description:
+	// 		"Yoga flow to help relieve fatigue, release stress, and support overall well-being.",
+	// 	booking: "https://kuyy.id/a6346c09-b758-4e28-b954-22baaf78e88a",
+	// },
+	// {
+	// 	id: 9,
+	// 	time: "2 February 2026 | 19.00–20.00 WITA",
+	// 	type: "After Work Yoga",
+	// 	level: "All Levels",
+	// 	instructor: "Saut",
+	// 	duration: 60,
+	// 	description:
+	// 		"Yoga session focused on reducing tiredness and releasing stress after a long day.",
+	// 	booking: "https://kuyy.id/c66b6889-e369-4d27-9def-8251a39acea4",
+	// },
 	{
 		id: 12,
-		time: "3 February 2026 | 18.00–19.15 WITA",
+		time: "03 Feb 2026 | 18.00–19.15 WITA",
 		type: "Gentle Evening Flow",
 		level: "All Levels",
 		instructor: "Tatjana",
@@ -54,5 +54,35 @@ export const classes = [
 		description:
 			"Gentle vinyasa style all body flow suitable for all levels. Includes 75 minutes of flow, 10% discount on all NUI Healthy Food menus, and yoga mats available in limited quantity.",
 		booking: "https://kuyy.id/58694fd3-eb54-4327-b45b-06faaaf8a27e",
+	},
+	{
+		id: 13,
+		time: "04 Feb 2026 | 18:00-19:15 WITA",
+		type: "After Office Vinyasa with Tatjana",
+		level: "All Levels",
+		instructor: "Tatjana",
+		duration: 75,
+		description: `Vinyasa style all body flow, led with Tatjana.
+This class is for all levels yoga practitioners.
+What you’ll get:
+75 minutes vinyasa all body flow
+10% discount in NUI Healthy food for all menus
+Mats are available in limited quantity💓`,
+		booking: "https://kuyy.id/58694fd3-eb54-4327-b45b-06faaaf8a27e",
+	},
+	{
+		id: 13,
+		time: "07 Feb 2026 | 08:00-09:15 WITA",
+		type: "All Body Flow with Tatjana",
+		level: "All Levels",
+		instructor: "Tatjana",
+		duration: 75,
+		description: `Embracing vinyasa style all body flow, led with Tatjana.
+This class is for all levels yoga practitioners.
+What you’ll get:
+75 minutes vinyasa all body flow
+10% discount in NUI Healthy food for all menus
+Mats are available in limited quantity💓`,
+		booking: "https://kuyy.id/8a98dad2-1a85-48f9-b6fb-ce3cc85e3dbe,",
 	},
 ];
